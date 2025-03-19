f4bd7aefaf310d01738ddad45de9b34fa501e48d
Bai-Chiang <contact@bai-chiang.com>
2025-03-18 17:35:03 -0700

    Update test script

--- files/test_scripts/test.sh
+++ files/test_scripts/test.sh
@@ -3,12 +3,15 @@
 #SBATCH --hint=nomultithread
 #SBATCH --output=stdout
 
-#srun -n1 -l bash -c "cat /etc/hostname"
+echo -e "\n\nsrun -n1 -l bash -c \"cat /etc/hostname\""
+srun -n1 -l bash -c "cat /etc/hostname"
 
+echo -e "\n\ncat /etc/hostname"
 cat /etc/hostname
 
+echo -e "\n\nsrun -l cat /etc/hostname"
 srun -l cat /etc/hostname
 
 sleep 1
-
+echo -e "\n\n"'$CONDA_PREFIX/bin/mpiexec -n 4 $CONDA_PREFIX/bin/python test.py'
 $CONDA_PREFIX/bin/mpiexec -n 4 $CONDA_PREFIX/bin/python test.py 
